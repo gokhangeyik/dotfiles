66410979418508f549a4fad83486d582f23c2f91
Gökhan Geyik <contact@gokhangeyik.net>
2025-01-08 20:10:59 +0100

    cleanup(nvim): config cleaning

--- nvim/lua/config/GokkoNvim.lua
+++ nvim/lua/config/GokkoNvim.lua
@@ -110,21 +110,4 @@ _GokkoNvim.flat_floats = function(colors, titles)
   })
 end
 
--- Copied from NeoVim for code folding
--- _GokkoNvim.foldexpr = function()
---   local buf = vim.api.nvim_get_current_buf()
---   if vim.b[buf].ts_folds == nil then
---     -- as long as we don't have a filetype, don't bother
---     -- checking if treesitter is available (it won't)
---     if vim.bo[buf].filetype == "" then
---       return "0"
---     end
---     if vim.bo[buf].filetype:find("dashboard") then
---       vim.b[buf].ts_folds = false
---     else
---       vim.b[buf].ts_folds = pcall(vim.treesitter.get_parser, buf)
---     end
---   end
---   return vim.b[buf].ts_folds and vim.treesitter.foldexpr() or "0"
--- end
 return _GokkoNvim
