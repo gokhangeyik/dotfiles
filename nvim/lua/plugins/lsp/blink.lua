45a856a6dac07e675654724fc99605fa2fbceef3
Gökhan Geyik <contact@gokhangeyik.net>
2025-01-09 18:18:14 +0100

    config(nvim): add emoji source to blink.cmp

--- nvim/lua/plugins/lsp/blink.lua
+++ nvim/lua/plugins/lsp/blink.lua
@@ -5,6 +5,7 @@ return {
   -- lazy = false, -- lazy loading handled internally
   dependencies = {
     "rafamadriz/friendly-snippets",
+    "moyiz/blink-emoji.nvim",
     { "L3MON4D3/LuaSnip", version = "v2.*" },
   },
   version = "v0.*",
@@ -26,9 +27,37 @@ return {
       },
       keymap = { preset = "super-tab" },
       sources = {
-        default = { "lsp", "path", "buffer", "snippets" },
+        default = { "lsp", "path", "buffer", "snippets", "emoji" },
         -- optionally disable cmdline completions
         -- cmdline = {},
+        providers = {
+          lsp = {
+            name = "LSP",
+            module = "blink.cmp.sources.lsp",
+            opts = {}, -- Passed to the source directly, varies by source
+
+            --- NOTE: All of these options may be functions to get dynamic behavior
+            --- See the type definitions for more information
+            enabled = true, -- Whether or not to enable the provider
+            async = true, -- Whether we should wait for the provider to return before showing the completions
+            timeout_ms = 2000, -- How long to wait for the provider to return before showing completions and treating it as asynchronous
+            transform_items = nil, -- Function to transform the items before they're returned
+            should_show_items = true, -- Whether or not to show the items
+            max_items = nil, -- Maximum number of items to display in the menu
+            min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
+            -- If this provider returns 0 items, it will fallback to these providers.
+            -- If multiple providers falback to the same provider, all of the providers must return 0 items for it to fallback
+            fallbacks = {},
+            score_offset = 0, -- Boost/penalize the score of the items
+            override = nil, -- Override
+          },
+          emoji = {
+            module = "blink-emoji",
+            name = "Emoji",
+            score_offset = 15, -- Tune by preference
+            opts = { insert = true }, -- Insert emoji (default) or complete its name
+          },
+        },
       },
       completion = {
         menu = {
