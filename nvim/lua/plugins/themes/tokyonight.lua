605a0979ff3821aed01005ab5d42202251f65912
Gökhan Geyik <contact@gokhangeyik.net>
2025-01-19 14:50:58 +0100

    config(k9s): enable tokyonight skin

--- nvim/lua/plugins/themes/tokyonight.lua
+++ nvim/lua/plugins/themes/tokyonight.lua
@@ -12,14 +12,14 @@ return {
 
       style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
       light_style = "day", -- The theme is used when the background is set to light
-      transparent = true, -- Enable this to disable setting the background color
+      transparent = false, -- Enable this to disable setting the background color
       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
       styles = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value for `:help nvim_set_hl`
         comments = { italic = true },
         keywords = { italic = true },
-        functions = { bold = true, italic = false },
+        functions = { bold = false, italic = false },
         variables = {},
         -- Background styles. Can be "dark", "transparent" or "normal"
         sidebars = "normal", -- style for sidebars, see below
