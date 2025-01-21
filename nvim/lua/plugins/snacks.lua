839c636522241226ea28fba353f080c8a401e649
Gökhan Geyik <contact@gokhangeyik.net>
2025-01-21 13:24:21 +0100

    config(nvim.todo-comments): quick fix for snacks picker

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -43,6 +43,21 @@ return {
     -- },
   },
   keys = {
+
+    {
+      "<leader>st",
+      function()
+        Snacks.picker.todo_comments()
+      end,
+      desc = "Todo",
+    },
+    {
+      "<leader>sT",
+      function()
+        Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+      end,
+      desc = "Todo/Fix/Fixme",
+    },
     {
       "<leader>su",
       function()
