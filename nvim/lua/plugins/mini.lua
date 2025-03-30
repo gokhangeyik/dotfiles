a588dfacf89ce45c0c45db938c4fe8bff0a355a8
Gökhan Geyik <contact@gokhangeyik.net>
2025-03-30 14:52:41 +0200

    chore(nvim/mini): add statusline config

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,6 +5,37 @@ return {
   enabled = true,
   version = "*",
   config = function()
+    -- local active = function()
+    --   -- *This* is the place that gets adjusted
+    --   local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 100 })
+    --   local git = MiniStatusline.section_git({ trunc_width = 40 })
+    --   local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+    --   local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+    --   local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+    --   local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+    --   local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+    --   local location = MiniStatusline.section_location({ trunc_width = 75 })
+    --   local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+    --
+    --   return MiniStatusline.combine_groups({
+    --     { hl = mode_hl, strings = { mode } },
+    --     { strings = { "" } },
+    --     { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+    --     "%<", -- Mark general truncate point
+    --     { hl = "MiniStatuslineFilename", strings = { filename } },
+    --     "%=", -- End left alignment
+    --     { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+    --     { hl = mode_hl, strings = { search, location } },
+    --   })
+    -- end
+    -- local mini_statusline = require("mini.statusline")
+    -- mini_statusline.setup({
+    --   content = { active = active },
+    --   use_icons = true,
+    --   set_vim_settings = true,
+    -- })
+    -- require("mini.statusline").setup({})
+    -- vim.cmd("set laststatus=3")
     require("mini.ai").setup({
       -- Table with textobject id as fields, textobject specification as values.
       -- Also use this to disable builtin textobjects. See |MiniAi.config|.
