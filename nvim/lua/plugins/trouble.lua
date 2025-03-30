6f07adb7b057cc8decddf19c5d428818f8c0efaa
Gökhan Geyik <contact@gokhangeyik.net>
2025-03-30 14:48:10 +0200

    chore(nvim/trouble): disable plugin

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -2,7 +2,7 @@ return {
   "folke/trouble.nvim",
   -- event = "VeryLazy",
   lazy = true,
-  enabled = true,
+  enabled = false,
   opts = {}, -- for default options, refer to the configuration section for custom setup.
   cmd = "Trouble",
   keys = {
