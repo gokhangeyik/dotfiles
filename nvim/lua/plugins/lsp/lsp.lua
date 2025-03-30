a741307e086bb72350dc636b981c1880ab4ab44f
Gökhan Geyik <contact@gokhangeyik.net>
2025-03-30 14:52:06 +0200

    chore(nvim/lsp): remove unused functions

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -77,26 +77,26 @@ return {
         end
         -- /LSP Config Exceptions
 
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-          local highlight_augroup = vim.api.nvim_create_augroup("gokko-lsp-highlight", { clear = false })
-          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.document_highlight,
-          })
-          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.clear_references,
-          })
-          vim.api.nvim_create_autocmd("LspDetach", {
-            group = vim.api.nvim_create_augroup("gokko-lsp-detach", { clear = true }),
-            callback = function(event2)
-              vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds({ group = "gokko-lsp-highlight", buffer = event2.buf })
-            end,
-          })
-        end
+        -- if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+        --   local highlight_augroup = vim.api.nvim_create_augroup("gokko-lsp-highlight", { clear = false })
+        --   vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        --     buffer = event.buf,
+        --     group = highlight_augroup,
+        --     callback = vim.lsp.buf.document_highlight,
+        --   })
+        --   vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+        --     buffer = event.buf,
+        --     group = highlight_augroup,
+        --     callback = vim.lsp.buf.clear_references,
+        --   })
+        --   vim.api.nvim_create_autocmd("LspDetach", {
+        --     group = vim.api.nvim_create_augroup("gokko-lsp-detach", { clear = true }),
+        --     callback = function(event2)
+        --       vim.lsp.buf.clear_references()
+        --       vim.api.nvim_clear_autocmds({ group = "gokko-lsp-highlight", buffer = event2.buf })
+        --     end,
+        --   })
+        -- end
       end,
     })
   end,
