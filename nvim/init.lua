bb70f761114a53a88e265a122f1ce8c965c20fe5
Gökhan Geyik <contact@gokhangeyik.net>
2025-03-30 14:47:25 +0200

    feat(nvim): disable module cache

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,18 @@
-local function optimize_lua_cache()
-  local package_cache = {}
-  local old_require = require
-
-  require = function(name)
-    if package_cache[name] then
-      return package_cache[name]
-    end
-    local loaded = old_require(name)
-    package_cache[name] = loaded
-    return loaded
-  end
-end
-
-optimize_lua_cache()
-
+-- local function optimize_lua_cache()
+--   local package_cache = {}
+--   local old_require = require
+--
+--   require = function(name)
+--     if package_cache[name] then
+--       return package_cache[name]
+--     end
+--     local loaded = old_require(name)
+--     package_cache[name] = loaded
+--     return loaded
+--   end
+-- end
+-- optimize_lua_cache()
+--
 _GokkoNvim = require("config.GokkoNvim")
 _GokkoNvim.async(_GokkoNvim.init_deps)
 require("config.options")
