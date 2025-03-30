75e1cfb0dfe737466625c58b5fc7921959d06a7d
Gökhan Geyik <contact@gokhangeyik.net>
2025-03-30 15:07:06 +0200

    feat(nvim/lazy): add dev context

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -42,6 +42,15 @@ require("lazy").setup({
   defaults = {
     lazy = true,
   },
+  dev = {
+    -- Directory where you store your local plugin projects. If a function is used,
+    -- the plugin directory (e.g. `~/projects/plugin-name`) must be returned.
+    ---@type string | fun(plugin: LazyPlugin): string
+    path = "~/Documents/Devel/neovim/",
+    ---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
+    patterns = {}, -- For example {"folke"}
+    fallback = false, -- Fallback to git when local plugin doesn't exist
+  },
   spec = {
     -- import your plugins
     { import = "plugins.themes" },
