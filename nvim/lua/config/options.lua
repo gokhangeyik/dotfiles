19c02cbf165725dc576df77b31abd2a89c60cefc
Gökhan Geyik <contact@gokhangeyik.net>
2025-03-30 14:50:18 +0200

    style(nvim): change options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,73 +1,75 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
 vim.g.kanagawa_lualine_bold = true
 
-local opt = vim.opt
-opt.numberwidth = 2
-opt.shortmess:append("sI")
-opt.pumheight = 15
-opt.swapfile = false
-opt.smoothscroll = true
-opt.clipboard = "unnamedplus" -- Sync with system clipboard
-opt.completeopt = { "menu", "menuone", "noselect" }
-opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
-opt.cursorline = true -- Enable highlighting of the current line
-opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.numberwidth = 2
+vim.opt.shortmess:append("sI")
+vim.opt.pumheight = 15
+vim.opt.swapfile = false
+vim.opt.smoothscroll = true
+vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
+vim.opt.confirm = true -- Confirm to save changes before exiting modified buffer
+vim.opt.cursorline = true -- Enable highlighting of the current line
+vim.opt.expandtab = true -- Use spaces instead of tabs
 -- opt.foldlevel = 1
 -- opt.foldcolumn = "1"
 -- opt.foldlevelstart = 99
 -- opt.foldmethod = "expr"
 -- opt.foldexpr = "nvim_treesitter#foldexpr()"
-opt.foldcolumn = "1" -- '0' is not bad
-opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-opt.foldlevelstart = 99
-opt.foldenable = true
-opt.fillchars = "eob: " -- Hide end of buffer character `~`
-opt.ignorecase = true -- Ignore case
-opt.inccommand = "nosplit" -- preview incremental substitute
-opt.jumpoptions = "view"
-opt.laststatus = 3 -- global statusline
-opt.linebreak = true -- Wrap lines at convenient points
-opt.list = true -- Show some invisible characters (tabs...
-opt.mouse = "a" -- Enable mouse mode
-opt.number = true -- Print line number
-opt.pumblend = 10 -- Popup blend
-opt.pumheight = 10 -- Maximum number of entries in a popup
-opt.relativenumber = true -- Relative line numbers
-opt.ruler = false -- Disable the default ruler
-opt.scrolloff = 10 -- Lines of context
-opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
-opt.shiftround = true -- Round indent
-opt.shiftwidth = 2 -- Size of an indent
-opt.shortmess:append({ W = true, I = true, c = true, C = true })
-opt.showmode = false -- Dont show mode since we have a statusline
-opt.sidescrolloff = 10 -- Columns of context
-opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true -- Don't ignore case with capitals
-opt.smartindent = true -- Insert indents automatically
-opt.spelllang = { "en" }
-opt.splitbelow = true -- Put new windows below current
-opt.splitkeep = "screen"
-opt.splitright = true --
-opt.tabstop = 2 -- Number of spaces tabs count for
-opt.softtabstop = 2
-opt.termguicolors = true -- True color support
--- opt.guicursor
+-- here
+vim.opt.foldcolumn = "1" -- '0' is not bad
+vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
+-- /here
+
+vim.opt.fillchars = "eob: " -- Hide end of buffer character `~`
+vim.opt.ignorecase = true -- Ignore case
+vim.opt.inccommand = "nosplit" -- preview incremental substitute
+vim.opt.jumpoptions = "view"
+vim.opt.laststatus = 3 -- global statusline
+vim.opt.linebreak = true -- Wrap lines at convenient points
+vim.opt.list = true -- Show some invisible characters (tabs...
+vim.opt.mouse = "a" -- Enable mouse mode
+vim.opt.number = true -- Print line number
+vim.opt.pumblend = 10 -- Popup blend
+vim.opt.pumheight = 10 -- Maximum number of entries in a popup
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.ruler = false -- Disable the default ruler
+vim.opt.scrolloff = 10 -- Lines of context
+vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+vim.opt.shiftround = true -- Round indent
+vim.opt.shiftwidth = 2 -- Size of an indent
+vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
+vim.opt.showmode = false -- Dont show mode since we have a statusline
+vim.opt.sidescrolloff = 10 -- Columns of context
+vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+vim.opt.smartcase = true -- Don't ignore case with capitals
+vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.spelllang = { "en" }
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitkeep = "screen"
+vim.opt.splitright = true --
+vim.opt.tabstop = 2 -- Number of spaces tabs count for
+vim.opt.softtabstop = 2
+vim.opt.termguicolors = true -- True color support
+-- vim.opt.guicursor
 --   "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
-opt.timeoutlen = 700 -- Lower than default (1000) to quickly trigger which-key
-opt.ttimeoutlen = 50
-opt.undofile = true
-opt.undolevels = 10000
-opt.updatetime = 500 -- Save swap file and trigger CursorHold
-opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
-opt.wildmode = "longest:full,full" -- Command-line completion mode
-opt.winminwidth = 5 -- Minimum window width
-opt.wrap = false -- Disable line wrap
+vim.opt.timeoutlen = 700 -- Lower than default (1000) to quickly trigger which-key
+vim.opt.ttimeoutlen = 50
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 500 -- Save swap file and trigger CursorHold
+vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
+vim.opt.winminwidth = 5 -- Minimum window width
+vim.opt.wrap = false -- Disable line wrap
 
 vim.diagnostic.config({
   virtual_text = false, -- floating text next to code is too noisy.
+  virtual_lines = { current_line = true },
 
   -- virtual_text = {
   --   spacing = 4,
