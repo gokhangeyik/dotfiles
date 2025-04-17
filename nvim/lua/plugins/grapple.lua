638383c0a36d950e617e316af018a92285de4d53
Gökhan Geyik <contact@gokhangeyik.net>
2025-04-17 08:10:51 +0200

    chore(nvim/grapple): update keymaps

--- nvim/lua/plugins/grapple.lua
+++ nvim/lua/plugins/grapple.lua
@@ -6,15 +6,13 @@ return {
     status = true,
   },
   keys = {
-    { "<M-=>", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
-    { "<M-0>", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
-
-    { "<M-1>", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
-    { "<M-2>", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
-    { "<M-3>", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
-    { "<M-4>", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
-    { "<M-5>", "<cmd>Grapple select index=5<cr>", desc = "Select fifth tag" },
-
+    { "<a-=>", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
+    { "<a-0>", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
+    { "<a-1>", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
+    { "<a-2>", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
+    { "<a-3>", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
+    { "<a-4>", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+    { "<a-5>", "<cmd>Grapple select index=5<cr>", desc = "Select fifth tag" },
     -- { "<c-s-n>", "<cmd>Grapple cycle_tags next<cr>", desc = "Go to next tag" },
     -- { "<c-s-p>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Go to previous tag" },
   },
