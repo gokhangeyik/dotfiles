4c1b92522c313a178716ad4408515b3225de7715
Gökhan Geyik <contact@gokhangeyik.net>
2025-04-22 23:15:06 +0200

    refactor(nvim/lsp): remove unused keymaps

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -1,8 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-  -- event = "VeryLazy",
-  -- lazy = true,
   enabled = true,
   dependencies = {
     { "williamboman/mason.nvim", config = true },
@@ -47,22 +45,7 @@ return {
           mode = mode or "n"
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
         end
-        -- map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-        -- map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-        -- map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-        -- map("<leader>lD", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-        -- map("<leader>ls", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-        -- map("<leader>lS", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-        -- map("gd", "<cmd>FzfLua lsp_definitions<cr>", "[G]oto [D]efinition")
-        -- map("gr", "<cmd>FzfLua lsp_references<cr>", "[G]oto [R]eferences")
-        -- map("gI", "<cmd>FzfLua lsp_implementations<cr>", "[G]oto [I]mplementation")
-        -- map("<leader>lD", "<cmd>FzfLua lsp_typedefs<cr>", "Type [D]efinition")
-        -- map("<leader>ls", "<cmd>FzfLua lsp_document_symbols<cr>", "[D]ocument [S]ymbols")
-        -- map("<leader>lS", "<cmd>FzfLua lsp_dynamic_workspace_symbols<cr>", "[W]orkspace [S]ymbols")
         map("<leader>lr", vim.lsp.buf.rename, "[R]e[n]ame")
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
         map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
