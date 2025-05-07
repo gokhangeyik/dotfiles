dc6bfdbf9a6b7be7d88339e2b5c2d13be1aad143
Gökhan Geyik <contact@gokhangeyik.net>
2025-05-07 09:14:28 +0200

    chore(nvim/gitsigns): change indicators

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,37 +3,21 @@ return {
   event = "VeryLazy",
   enabled = true,
   opts = {
-    -- signs = {
-    --   add = { text = "┃" },
-    --   change = { text = "┃" },
-    --   delete = { text = "_" },
-    --   topdelete = { text = "‾" },
-    --   changedelete = { text = "~" },
-    --   untracked = { text = "┆" },
-    -- },
-    -- signs_staged = {
-    --   add = { text = "┃" },
-    --   change = { text = "┃" },
-    --   delete = { text = "_" },
-    --   topdelete = { text = "‾" },
-    --   changedelete = { text = "~" },
-    --   untracked = { text = "┆" },
-    -- },
-    signs = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "▎" },
-      untracked = { text = "▎" },
-    },
     signs_staged = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "▎" },
+      add = { text = "┃" },
+      change = { text = "┃" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
     },
+    -- signs_staged = {
+    --   add = { text = "▎" },
+    --   change = { text = "▎" },
+    --   delete = { text = "" },
+    --   topdelete = { text = "" },
+    --   changedelete = { text = "▎" },
+    -- },
     signs_staged_enable = true,
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -47,8 +31,8 @@ return {
     current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
       virt_text = true,
-      virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
-      delay = 500,
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      delay = 100,
       ignore_whitespace = false,
       virt_text_priority = 100,
       use_focus = true,
