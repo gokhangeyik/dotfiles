9aed4fbdcd0f9762e136deb6e09eb573f14759a4
Gökhan Geyik <contact@gokhangeyik.net>
2025-05-20 18:51:43 +0200

    chore(nvim/autocmds): disable `focus` for diagnostics float

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -24,7 +24,7 @@ autocmd({ "BufWritePre" }, {
 -- Show errors and warnings in a floating window
 vim.api.nvim_create_autocmd("CursorHold", {
   callback = function()
-    vim.diagnostic.open_float(nil, { focusable = true, source = "if_many" })
+    vim.diagnostic.open_float(nil, { focusable = false, source = "if_many" })
   end,
 })
 --
