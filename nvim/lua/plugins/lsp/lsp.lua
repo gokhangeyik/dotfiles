b5d7cc57d9782f43ebabe895f6d453228accc7e6
Gökhan Geyik <contact@gokhangeyik.net>
2025-06-02 09:24:08 +0200

    fix(nvim): nvim 0.11.2 fix?

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -39,7 +39,7 @@ return {
   -- },
   config = function()
     for lsp_name, _ in pairs(_GokkoNvim.lsp) do
-      vim.lsp.enable(lsp_name)
+      -- vim.lsp.enable(lsp_name)
       vim.lsp.config(lsp_name, _GokkoNvim.lsp[lsp_name])
     end
     vim.api.nvim_create_autocmd("LspAttach", {
@@ -49,8 +49,8 @@ return {
           mode = mode or "n"
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
         end
-        map("<leader>lr", vim.lsp.buf.rename, "[R]e[n]ame")
-        map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+        map("<leader>lr", vim.lsp.buf.rename, "Rename")
+        map("<leader>ca", vim.lsp.buf.code_action, "Code Actions", { "n", "x" })
 
         -- LSP Config Exceptions
         local client = vim.lsp.get_client_by_id(event.data.client_id) or {}
